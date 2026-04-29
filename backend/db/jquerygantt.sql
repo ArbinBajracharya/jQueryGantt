ea97ca0e4d52ce8c5f7ebed8189fa96031d9fb01
Arbin Bajracharya <arbinbighero6@gmail.com>
2026-04-29 17:08:00 +0545

    dev-arbin db updated

--- backend/db/jquerygantt.sql
+++ backend/db/jquerygantt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2026 at 12:44 PM
+-- Generation Time: Apr 29, 2026 at 01:22 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,6 +34,7 @@ CREATE TABLE `task` (
   `end` date NOT NULL,
   `dur` int(11) NOT NULL,
   `progress` int(11) NOT NULL,
+  `status` varchar(100) NOT NULL,
   `descript` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
@@ -41,13 +42,17 @@ CREATE TABLE `task` (
 -- Dumping data for table `task`
 --
 
-INSERT INTO `task` (`id`, `name`, `start`, `end`, `dur`, `progress`, `descript`) VALUES
-(1, 'Project Planning', '2026-05-01', '2026-05-03', 3, 100, 'Initial planning and requirement gathering'),
-(2, 'Database Design', '2026-05-04', '2026-05-06', 3, 80, 'Design tables and relationships for the system'),
-(3, 'Backend Development', '2026-05-07', '2026-05-15', 9, 60, 'Develop PHP APIs and business logic'),
-(4, 'Frontend Integration', '2026-05-16', '2026-05-20', 5, 40, 'Connect frontend with backend APIs'),
-(5, 'Testing & Bug Fixing NEW', '2026-05-21', '2026-05-25', 3, 20, 'TEST'),
-(7, 'NEW', '2026-04-01', '2026-04-03', 4, 10, 'TEST');
+INSERT INTO `task` (`id`, `name`, `start`, `end`, `dur`, `progress`, `status`, `descript`) VALUES
+(1, 'Project Planning', '2026-07-13', '2027-02-02', 147, 0, 'STATUS_SUSPENDED', 'Initial planning and requirement gathering'),
+(2, 'Database Design', '2026-04-17', '2026-06-22', 47, 100, 'STATUS_DONE', 'Design tables and relationships for the system'),
+(3, 'Backend Development', '2026-05-07', '2026-05-19', 9, 60, 'STATUS_FAILED', 'Develop PHP APIs and business logic'),
+(4, 'Frontend Integration', '2026-05-18', '2026-05-22', 5, 40, 'STATUS_SUSPENDED', 'Connect frontend with backend APIs'),
+(5, 'Testing & Bug Fixing NEW', '2026-05-21', '2026-05-25', 3, 20, 'STATUS_ACTIVE', 'TEST'),
+(8, 'New', '2026-04-03', '2026-04-08', 4, 0, 'STATUS_ACTIVE', 'TEST'),
+(9, 'Test', '2026-04-20', '2026-04-25', 4, 100, 'STATUS_DONE', 'Test'),
+(10, 'Hello', '2026-04-01', '2026-04-17', 14, 1, 'STATUS_FAILED', 'Test'),
+(11, 'New', '2026-04-01', '2026-04-17', 13, 10, 'STATUS_ACTIVE', 'New'),
+(12, 'test', '2026-04-01', '2026-04-17', 13, 10, 'STATUS_ACTIVE', 'test');
 
 --
 -- Indexes for dumped tables
@@ -67,7 +72,7 @@ ALTER TABLE `task`
 -- AUTO_INCREMENT for table `task`
 --
 ALTER TABLE `task`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
